47bba26eecca8da0ac305c68827dba517a79e85d
Sandhya <Sandhya@DESKTOP-VA82HA3>
2026-02-21 14:11:00 +0530

    first comment1

---
+++ src/test/java/webDriver/firstprog.java
@@ -0,0 +1,20 @@
+package webDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstprog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com/");
+		
+		
+
+	}
+
+}
